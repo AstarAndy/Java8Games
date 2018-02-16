89c29b68f5332fd3542faff938b3028b0e33c623
Andy Faust <andyfaust@Andys-MacBook-Pro.local>
2018-02-16 14:07:31 -0600

    Added a main section and a readme.md

---
+++ readme.md
@@ -0,0 +1,9 @@
+#Java 8 Games
+
+This project contains a collections of very basic java classes that illustrate some basic java 8 stuff.  See below for each class and what it does.
+
+### Duration Games
+
+This class illustrates the use of `TimeUnit` and `Instant` and `Duration` java 8 classes.
+
+
--- src/main/java/com/astar/andy/DurationGames.java
+++ src/main/java/com/astar/andy/DurationGames.java
@@ -4,6 +4,13 @@ import java.util.concurrent.TimeUnit;
 
 public class DurationGames {
 
+    public static void main(String ... args) {
+        System.out.println("Started.  Gonna wait 2 seconds and then stop.");
+        DurationGames dg = new DurationGames();
+        dg.forceDelayOf(TimeUnit.SECONDS, 2);
+        System.out.println("That's a wrap.");
+    }
+
     public void forceDelayOf(TimeUnit unitOfTime, int howLong) {
         try {
             unitOfTime.sleep(howLong);
