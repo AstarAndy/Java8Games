dad6785f48c01b5ce719428ed336785a3027fcf6
Andy Faust <andyfaust@Andys-MacBook-Pro.local>
2018-02-16 11:50:12 -0600

    Cleanup ans simple duration test

--- src/main/java/com/astar/andy/DurationGames.java
+++ src/main/java/com/astar/andy/DurationGames.java
@@ -1,5 +1,14 @@
 package com.astar.andy;
 
+import java.util.concurrent.TimeUnit;
+
 public class DurationGames {
 
+    public void forceDelayOf(TimeUnit unitOfTime, int howLong) {
+        try {
+            unitOfTime.sleep(howLong);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/test/java/com/astar/andy/DurationGamesTest.java
+++ src/test/java/com/astar/andy/DurationGamesTest.java
@@ -1,8 +1,9 @@
 package com.astar.andy;
 
-import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
 
@@ -12,8 +13,9 @@ public class DurationGamesTest {
     public void Duration_Test_For_3_second_Delay() {
         Instant startedAt = Instant.now();
         DurationGames dg = new DurationGames();
-        dg.forceDelayOf(TimeUnit.SECONDS, 5);
-
+        dg.forceDelayOf(TimeUnit.SECONDS, 3);
+        Duration elpTime = Duration.between(startedAt, Instant.now());
+        assertEquals(elpTime.getSeconds(), 3);
 
     }
 
